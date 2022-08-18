1481d0d7778f93fab94e7f7497ca22d11fbcb179
ns-cn <ns-cn@qq.com>
2022-08-18 11:08:31 +0800

    usage

--- list.go
+++ list.go
@@ -11,7 +11,13 @@ var cmdList = &cobra.Command{
 	Use:     "list",
 	Aliases: []string{"l"},
 	Short:   "查询变更",
-	Long:    `查询svn提交记录的文件变更`,
+	Long: `功能： 【查询svn提交记录的文件变更】
+必要参数：源分支branch(b)、版本范围（last(l)或revision(r)）
+查看仓库最近10次提交： muddler list -b ./code/svn -l 10
+查看仓库指定版本号的提交： muddler list -b ./code/svn -r 1024
+查看仓库指定版本区间的提交： muddler list -b ./code/svn -r 1024:2048
+可选增加
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fileUpdates, err := svn.Log(branch, authors, revision, last)
 		if err != nil {
@@ -30,8 +36,8 @@ var cmdList = &cobra.Command{
 }
 
 func RegCmdList() {
-	cmdList.Flags().StringSliceVarP(&authors, "authors", "a", []string{}, "filter by authors")
-	cmdList.Flags().StringVarP(&branch, "branch", "b", ".", "source branch")
+	cmdList.Flags().StringSliceVarP(&authors, "authors", "a", []string{}, "通过提交者过滤（暂未生效）")
+	cmdList.Flags().StringVarP(&branch, "branch", "b", ".", "源分支")
 	cmdList.Flags().StringVarP(&revision, "revision", "r", "", "版本，不指定则为所有，可选单次(1024)或范围(1024:2048)")
 	cmdList.Flags().IntVarP(&last, "last", "l", 0, "指定查看最近的多少次更新")
 	root.AddCommand(cmdList)
--- merge.go
+++ merge.go
@@ -21,7 +21,12 @@ var cmdMerge = &cobra.Command{
 	Use:     "merge",
 	Aliases: []string{"m"},
 	Short:   "合并变更",
-	Long:    `查询svn提交记录的文件变更并合并到指定目录`,
+	Long: `功能： 【查询svn提交记录的文件变更并合并到指定目录】
+必要参数：源分支branch(b)、目标分支target(t,可多选)、版本范围（last(l)或revision(r)）
+存放仓库最近10次提交到指定目录： muddler list -b ./code/svn -t ./update-20220818 -l 10
+合并指定分支版本到另外多个分支： muddler list -b ./code/svn -t ./branch1 -t ./branch2 -r 1024
+合并仓库指定版本区间的提交到另外一个分支： muddler list -b ./code/svn -r 1024:2048 -t ./branch1
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		workDir, err := os.Getwd()
 		if err != nil {
@@ -114,13 +119,13 @@ func getFilePath(parent, file string) (targetPath string) {
 }
 
 func RegCmdMerge() {
-	cmdMerge.Flags().StringSliceVarP(&authors, "authors", "a", []string{}, "filter by authors")
+	cmdMerge.Flags().StringSliceVarP(&authors, "authors", "a", []string{}, "通过提交者过滤（暂未生效）")
 	cmdMerge.Flags().StringVarP(&revision, "revision", "r", "", "版本，不指定则为所有，可选单次(1024)或范围(1024:2048)")
-	cmdMerge.Flags().IntVarP(&last, "last", "l", 0, "指定查看最近的多少次更新")
+	cmdMerge.Flags().IntVarP(&last, "last", "l", 0, "最近的多少次更新")
 
 	cmdMerge.Flags().StringVarP(&branch, "branch", "b", ".", "源分支")
 	cmdMerge.Flags().StringSliceVarP(&targets, "target", "t", []string{}, "目标分支")
 
-	cmdMerge.Flags().BoolVarP(&preview, "preview", "p", true, "预览所有的变更")
+	cmdMerge.Flags().BoolVarP(&preview, "preview", "p", true, "是否预览所有的变更，否则直接提交")
 	root.AddCommand(cmdMerge)
 }
