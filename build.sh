8a0d19258995e8a16093030a118b52311b534c29
ns-cn <ns-cn@qq.com>
2022-08-18 12:07:48 +0800

    skip windows support

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./built/muddler_linux .
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./built/muddler.exe .
+#CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./built/muddler.exe .
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./built/muddler_darwin .
